37bc56a94bcab5adf4be3b2e2a996e8be5787b59
Fabio Percivaldi <fabio.percivaldi@hotmail.it>
2020-05-30 23:19:00 +0200

    refactored tests

--- main.go
+++ main.go
@@ -6,6 +6,9 @@ const englishHelloPrefix = "Hello, "
 
 // Hello returns greeting to specific name
 func Hello(name string) string {
+	if name == "" {
+		return englishHelloPrefix + "World"
+	}
 	return englishHelloPrefix + name
 }
 
